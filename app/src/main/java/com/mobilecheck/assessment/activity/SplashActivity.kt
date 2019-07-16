1ff0dfcaa0b8b24b2ab6694debeaf44c1d1d632b
Sophia <sophia.sonam@ithands.net>
2019-07-16 15:48:34 +0530

    update:ic launcher

--- app/src/main/java/com/mobilecheck/assessment/activity/SplashActivity.kt
+++ app/src/main/java/com/mobilecheck/assessment/activity/SplashActivity.kt
@@ -1,9 +1,12 @@
 package com.mobilecheck.assessment.activity
 
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.view.View
+import android.view.WindowManager
 import com.mobilecheck.assessment.R
 
 class SplashActivity : AppCompatActivity() {
@@ -12,6 +15,12 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        } else {
+            val window = window
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        }
         Run.after(SPLASH_TIMEOUT.toLong()) {
             startActivity(Intent(this, MainActivity::class.java))
             finish()
