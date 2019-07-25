bd4b862cd49c41151a388ab38706fecc9d25c7dc
Sophia <sophia.sonam@ithands.net>
2019-07-25 11:51:08 +0530

    update:settings instructions to phone settings

--- app/src/main/java/com/mobilecheck/assessment/activity/DetailActivity.kt
+++ app/src/main/java/com/mobilecheck/assessment/activity/DetailActivity.kt
@@ -1,5 +1,6 @@
 package com.mobilecheck.assessment.activity
 
+import android.app.admin.DevicePolicyManager
 import android.database.Cursor
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
@@ -14,8 +15,8 @@ import com.mobilecheck.assessment.R
 import com.mobilecheck.assessment.database.DatabaseHandler
 import com.mobilecheck.assessment.model.SystemInfo
 import kotlinx.android.synthetic.main.activity_detail.*
-import kotlinx.android.synthetic.main.activity_detail.toolbar
-import kotlinx.android.synthetic.main.activity_main.*
+import android.content.Intent
+import android.provider.Settings
 
 class DetailActivity : AppCompatActivity() {
     private lateinit var systemInfo: SystemInfo
@@ -89,16 +90,37 @@ class DetailActivity : AppCompatActivity() {
             Constants().KEY_SUPPORT -> {
                 tvDescription.text = Constants().SUPPORT_DESCRIPTION
                 tvSettings.text = Constants().SUPPORT_SETTINGS
+                tvSettings.setOnClickListener {
+                    val updateIntent = Intent(Settings.ACTION_SETTINGS)
+                    startActivity(updateIntent)
+                }
             }
             Constants().KEY_SYSTEM_UPDATE -> {
-                tvDescription.text =Constants().SYSTEM_UPDATE_DESCRIPTION
+                tvDescription.text = Constants().SYSTEM_UPDATE_DESCRIPTION
                 ivSwitch.text = Constants().SYSTEM_UPDATE_MANUAL
                 tvSettings.text = Constants().SYSTEM_UPDATE_SETTINGS
+                tvSettings.setOnClickListener {
+                    val updateIntent = Intent(Settings.ACTION_SETTINGS)
+                    startActivity(updateIntent)
+                }
             }
             Constants().KEY_APP_UPDATE -> {
                 tvDescription.text = Constants().APP_UPDATE_DESCRIPTION
                 ivSwitch.text = Constants().APP_UPDATE_MANUAL
                 tvSettings.text = Constants().APP_UPDATE_SETTINGS
+                tvSettings.setOnClickListener {
+                    try {
+                        val intent = Intent("com.google.android.finsky.activities.SettingsActivity")
+                        intent.setClassName(
+                            "com.android.vending",
+                            "com.google.android.finsky.activities.SettingsActivity"
+                        )
+                        startActivity(intent)
+                    } catch (e: Exception) {
+                        e.printStackTrace()
+                    }
+
+                }
             }
             Constants().KEY_ROOTED -> {
                 tvDescription.text = Constants().ROOTED_DESCRIPTION
@@ -107,20 +129,42 @@ class DetailActivity : AppCompatActivity() {
             Constants().KEY_PASSWORD -> {
                 tvDescription.text = Constants().PASSWORD_DESCRIPTION
                 tvSettings.text = Constants().PASSWORD_SETTINGS
+                tvSettings.setOnClickListener {
+                    val passwordIntent = Intent(DevicePolicyManager.ACTION_SET_NEW_PASSWORD)
+                    startActivity(passwordIntent)
+                }
             }
             Constants().KEY_BACKUP -> {
                 tvDescription.text = Constants().BACKUP_DESCRIPTION
                 ivSwitch.text = Constants().BACKUP_MANUAL
-                tvSettings.text = Constants().BACKUP_SETTINGS //TODO do more research
+                tvSettings.text = Constants().BACKUP_SETTINGS
+                tvSettings.setOnClickListener {
+                    val backupIntent = Intent(Settings.ACTION_PRIVACY_SETTINGS)
+                    startActivity(backupIntent)
+                }
             }
             Constants().KEY_FIND_MY_DEVICE -> {
-                tvDescription.text =Constants().FIND_DEVICE_DESCRIPTION
+                tvDescription.text = Constants().FIND_DEVICE_DESCRIPTION
                 ivSwitch.text = Constants().FIND_DEVICE_MANUAL
                 tvSettings.text = Constants().FIND_DEVICE_SETTINGS
+                tvSettings.setOnClickListener {
+                     val deviceIntent = Intent(Settings.ACTION_SECURITY_SETTINGS)
+                     startActivity(deviceIntent)
+                }
             }
             Constants().KEY_VPN -> {
                 tvDescription.text = Constants().VPN_DESCRIPTION
                 tvSettings.text = Constants().VPN_SETTINGS
+                tvSettings.setOnClickListener {
+                    if (Build.VERSION.SDK_INT > Build.VERSION_CODES.N) {
+                        val backupIntent = Intent(Settings.ACTION_VPN_SETTINGS)
+                        startActivity(backupIntent)
+                    }else{
+                        val intent = Intent("android.net.vpn.SETTINGS")
+                        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                        startActivity(intent)
+                    }
+                }
             }
         }
     }
